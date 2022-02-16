a35d0d1a56c1062e4d0aae7fd614a732f49e5dd3
chinsan <clim1994@gmail.com>
2022-02-16 10:09:44 -0800

    created menu and drink cards
    co-authored: justin @thejustinlombardi@gmail.com
    co-authored: joey @joesphschang@gmail.com

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import "./App.css";
-import axios from "axios";
-import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Navigation from "./components/Navigation/Navigation";
-import LandingPage from "./components/LandingPage/LandingPage";
-import Menu from "./components/Menu/Menu";
-import About from "./components/About/About";
-import Profile from "./components/Profile/Profile";
-import Login from "./components/Login/Login";
-import SignUp from "./components/SignUp/SignUp";
-import DrinkCardDetails from "./components/DrinkCardDetails/DrinkCardDetails";
-import CreateDrink from "./components/CreateDrink/CreateDrink";
-import MyDrinks from "./components/MyDrinks/MyDrinks";
+import './App.css';
+import axios from 'axios';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { createContext, useState, useEffect } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import LandingPage from './components/LandingPage/LandingPage';
+import Menu from './components/Menu/Menu';
+import About from './components/About/About';
+import Profile from './components/Profile/Profile';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import DrinkCardDetails from './components/DrinkCardDetails/DrinkCardDetails';
+import CreateDrink from './components/CreateDrink/CreateDrink';
+import MyDrinks from './components/MyDrinks/MyDrinks';
 
-import { statesContext } from "./statesContext";
+// import { statesContext } from './statesContext';
+export const statesContext = createContext('');
 
 function App() {
-	const drinkUrl = "https://buildyobar.herokuapp.com/drinks/";
-	const [user, setUser] = useState({});
-	const [drinks, setDrinks] = useState([]);
+	const drinkUrl = 'https://buildyobar.herokuapp.com/drinks/';
+	const [user, setUser] = useState(false);
+	const [drinks, setDrinks] = useState(false);
 	const { pathname } = useLocation();
 
 	const getDrinks = async () => {
 		try {
 			const res = await axios.get(drinkUrl);
 			setDrinks(res.data);
-			console.log(drinks);
 		} catch (error) {
 			console.log(error);
 		}
@@ -36,20 +36,19 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
+		<div className='App'>
 			<statesContext.Provider value={{ drinks, setDrinks, user, setUser }}>
-				{pathname !== "/" && <Navigation></Navigation>}
-
+				{pathname !== '/' && <Navigation></Navigation>}
 				<Routes>
-					<Route path="/" element={<LandingPage />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/profile" element={<Profile />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/signup" element={<SignUp />} />
-					<Route path="/menu" element={<Menu drinks={drinks} />} />
-					<Route path="/menu/:id" element={<DrinkCardDetails />} />
-					<Route path="/createdrink" element={<CreateDrink />} />
-					<Route path="/mydrinks" element={<MyDrinks />} />
+					<Route path='/' element={<LandingPage />} />
+					<Route path='/about' element={<About />} />
+					<Route path='/profile' element={<Profile />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/signup' element={<SignUp />} />
+					<Route path='/menu' element={<Menu />} />
+					<Route path='/menu/:id' element={<DrinkCardDetails />} />
+					<Route path='/createdrink' element={<CreateDrink />} />
+					<Route path='/mydrinks' element={<MyDrinks />} />
 				</Routes>
 			</statesContext.Provider>
 		</div>
--- src/components/DrinkCard/DrinkCard.js
+++ src/components/DrinkCard/DrinkCard.js
@@ -1,12 +1,24 @@
 import { useContext } from 'react';
 import { statesContext } from '../../statesContext';
+import { Link } from 'react-router-dom';
 
-function DrinkCard(props) {
-	const { drinks, setDrinks, user, setUser } = useContext(statesContext);
-	
-	return <div>
-       
-    </div>;
+function DrinkCard({ drink }) {
+	// const { drinks, setDrinks, user, setUser } = useContext(statesContext);
+
+	return (
+		<div className='row'>
+			<div className='col-xs-12 d-flex'>
+				<Link to={`/menu/${drink.id}`}>
+					<div className='card'>
+						<img className='card-img-top' src={drink.photo} alt={drink.name} />
+						<div className='card-body'>
+							<p className='card-text'>{drink.name}</p>
+						</div>
+					</div>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default DrinkCard;
