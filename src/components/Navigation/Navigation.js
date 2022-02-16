dbc253518242efdce9c8360c571fbf77ddbba552
joesphchang <joesphschang@gmail.com>
2022-02-15 21:35:05 -0500

    Created Navigation links co-authored-by: justin @thejustinlombardi@gmail.com co-authored-by: chinsan @clim1994@gmail.com

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import LinkContainer from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Navigation(props) {
 	return (
 		<Navbar bg='light' expand='lg' collapseOnSelect={true}>
 			<Container>
-				<LinkContainer>
-					<Navbar.Brand href='#home'>BYOBar</Navbar.Brand>
-				</LinkContainer>
+				<Navbar.Brand as={Link} to='/'>BYOBar</Navbar.Brand>
 				<Navbar.Toggle aria-controls='basic-navbar-nav' />
 				<Navbar.Collapse id='basic-navbar-nav'>
 					<Nav className='me-auto'>
-						<LinkContainer to='/'>
-							<Nav.Link>Menu</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/profile'>
-							<Nav.Link>Profile</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/about'>
-							<Nav.Link>About</Nav.Link>
-						</LinkContainer>
-						<LinkContainer to='/login'>
-							<Nav.Link>Login</Nav.Link>
-						</LinkContainer>
+						<Nav.Link as={Link} to='/menu'>
+							Menu
+						</Nav.Link>
+						<Nav.Link as={Link} to='/profile'>
+							Profile
+						</Nav.Link>
+						<Nav.Link as={Link} to='/about'>
+							About
+						</Nav.Link>
+						<Nav.Link as={Link} to='/login'>
+							Login
+						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
