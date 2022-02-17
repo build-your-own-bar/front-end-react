fb0e4adb7af053ea84d039cc6550dd286ad97730
chinsan <clim1994@gmail.com>
2022-02-17 15:34:53 -0800

    styled drink cards
    co-authored-by: justin @thejustinlombardi@gmail.com
    co-authored-by: joey @joesphschang@gmail.com

--- src/components/DrinkCard/DrinkCard.js
+++ src/components/DrinkCard/DrinkCard.js
@@ -1,31 +1,23 @@
-import { useContext } from 'react';
-import { statesContext } from '../../statesContext';
 import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { Button, Row, Card} from 'react-bootstrap';
+import { Card, CardContent } from '@mui/material';
+import './DrinkCard.css';
 
 function DrinkCard({ drink }) {
 	// const { drinks, setDrinks, user, setUser } = useContext(statesContext);
 
 	return (
-		<Container>
-			<Row>
-				<div className='card-container '>
-					<Link to={`/menu/${drink.id}`}>
-						<div className='card'>
-							<img
-								className='card-img-top'
-								src={drink.photo}
-								alt={drink.name}
-							/>
-							<div className='card-body'>
-								<p className='card-text'>{drink.name}</p>
-							</div>
+		<Card elevation={3}>
+			<CardContent className='card-container'>
+				<Link to={`/menu/${drink.id}`}>
+					<div className='card'>
+						<img className='card-img-top' src={drink.photo} alt={drink.name} />
+						<div className='card-body'>
+							<p className='card-text'>{drink.name}</p>
 						</div>
-					</Link>
-				</div>
-			</Row>
-		</Container>
+					</div>
+				</Link>
+			</CardContent>
+		</Card>
 	);
 }
 
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,41 +1,50 @@
-import { useEffect, useContext } from "react";
-// import { Grid, Container } from '@mui/material';
-import axios from "axios";
-import { Link } from "react-router-dom";
-import DrinkCard from "../DrinkCard/DrinkCard";
-import { statesContext } from "../../App";
-import { Button, Row } from "react-bootstrap";
+import { useEffect, useContext } from 'react';
+import { Grid, Container } from '@mui/material';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import DrinkCard from '../DrinkCard/DrinkCard';
+import { statesContext } from '../../App';
+import { Button, Row } from 'react-bootstrap';
+import './Menu.css';
 
 function Menu() {
 	const { drinks, loggedIn } = useContext(statesContext);
 
+	const image = require('../../assets/shaker.png');
 	return (
-		<div>
-			<div>
-				<h2 className="text-warning text-center">Our Menu</h2>
-				<div className="drinkContainer">
-					{loggedIn && (
-						<div className="text-center mt-3">
-							<Link to="/createdrink/new">
-								<Button className="primary text-center">Create a drink</Button>
-							</Link>
-						</div>
-					)}
-				</div>
-				{drinks &&
-					drinks.map((drink, i) => (
-						<div
-							item
-							className="gridItem"
-							key={drink.id}
-							style={{
-								padding: "20px",
-								textDecoration: "none",
-							}}>
-							<DrinkCard drink={drink} />
-						</div>
-					))}
+		<div className='text-center'>
+			<br />
+			<img src={image} alt='shaker' classname='shaker' />
+			{/* credit to winnievinzence from flaticon.com */}
+			<br />
+			<br />
+			<h2 className='text-warning text-center'>Menu</h2>
+			<div className='text-center mt-3'>
+				<Link to='/createdrink/new'>
+					<Button className='btn btn-primary btn-md btn-block create-drink'>
+						Create a drink
+					</Button>
+				</Link>
 			</div>
+			<br />
+			<br />
+			<Container className='d-flex align-items-center justify-content-center'>
+				<Grid className='gridContainer' container spacing={0}>
+					{drinks &&
+						drinks.map((drink, i) => (
+							<div
+								item
+								className='gridItem'
+								key={drink.id}
+								style={{
+									padding: '20px',
+									textDecoration: 'none',
+								}}>
+								<DrinkCard drink={drink} />
+							</div>
+						))}
+				</Grid>
+			</Container>
 		</div>
 	);
 }
